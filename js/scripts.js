6ba6d97e787f2d12237522edfdbd202c64f0c893
David Lewis and Masen Matthews <masen@>
2018-01-11 11:59:35 -0800

    Add imgane of Michael Cera and add branch to show his image based upon age entered

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
-
-
+  $("#info").submit(function(event) {
+    var age = parseInt($("#Age").val());
+  if(age > 21) {
+    $("#result1").show();
+  } else {
+    alert("try again!");
+  }
+  event.preventDefault();
+});
 });
