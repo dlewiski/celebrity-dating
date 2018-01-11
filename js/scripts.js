62ed8013a3a627cde4957ffd19e98d73e323c846
David Lewis and Masen Matthews <masen@>
2018-01-11 13:37:24 -0800

    add rick harrison and barak obama rules yeahhhh

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
   $("#info").submit(function(event) {
     var age = parseInt($("#Age").val());
-  if(age > 21) {
-    $("#result1").show();
-  } else {
-    alert("try again!");
-  }
-  event.preventDefault();
-});
+    var gender = $("#gender").val();
+    var genre = $("#genre").val();
+
+    if(age > 21 && gender === "female" && genre === "Comedy") {
+      $("#result1").show();
+    } else if (age < 21) {
+      $("#result2").show();
+    } else if (gender === "male" && genre === "Action/Adventure") {
+      $("#result4").show();
+    } else {
+      alert("try again!");
+    }
+    event.preventDefault();
+  });
 });
